d167fef5f5be4fcda4755b9c68c336e6f26ac49f
Seba <seba@go@wp.pl>
2026-02-12 18:53:16 +0100

    init repo

---
+++ tests/login.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Login tests', () => {
+  test('login with correct credentials', async ({ page }) => {
+    await page.goto('https://demo-bank.vercel.app/');
+    await page.getByTestId('login-input').fill('testerlo');
+    await page.getByTestId('password-input').fill('haslo123');
+    await page.getByTestId('login-button').click();
+    await page.getByTestId('user-name').click();
+
+    await expect(page.getByTestId('user-name')).toHaveText('Jan Demobankowy');
+  });
+
+  test('login with incorrect credentials incorrect username', async ({ page }) => {
+    await page.goto('https://demo-bank.vercel.app/');
+    await page.getByTestId('login-input').fill('teser');
+    await page.getByTestId('password-input').click();
+
+    await expect(page.getByTestId('error-login-id')).toHaveText('identyfikator ma min. 8 znaków');
+  });
+ 
+  test('login with incorrect credentials incorrect password', async ({ page }) => {
+    await page.goto('https://demo-bank.vercel.app/');
+    await page.getByTestId('login-input').fill('testerlo');
+    await page.getByTestId('password-input').fill('sad');
+    await page.getByTestId('password-input').blur();
+
+    await expect(page.getByTestId('error-login-password')).toHaveText('hasło ma min. 8 znaków');
+  });
+});
